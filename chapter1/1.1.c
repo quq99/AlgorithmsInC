2187fdcadbb919942d872b3801bcf440e5741e7f
quq99 <eequq99@gmail.com>
2016-04-02 22:24:48 +0800

    Update 1.1.c
    add \n in scanf and printf.
    screen can print "they are already connected" if p q are connected.

---
+++ chapter1/1.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#define N 1000
+
+main()
+{
+	int i, p, q, t, id[N];
+	for (i = 0; i < N; i++)
+		{id[i] = i;}
+	while(scanf("%d %d", &p, &q) == 2)
+	{
+		if (id[p] == id[q])
+			{
+			printf("They are already connected!\n\n");
+			continue;
+			}
+		for (i = 0, t = id[p]; i < N; i++)
+		{
+			if(id[i] == t)
+				{id[i] = id[q];}
+		}
+		printf("%d %d\n\n", p, q);
+	}	
+
+}
